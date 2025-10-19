4aff1961765444054444d1222e7d6433bb5cdcb8
Nandyalasuryanaraya <sridavidnandyala@gmail.com>
2025-10-19 15:44:09 +0530

    c18

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,15 +1,17 @@
 # from django.http import HttpResponse
-
 from django.shortcuts import render
 from .forms import BookingForm
 # from .models import Menu
+
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def book(request):
     form = BookingForm()
@@ -20,4 +22,6 @@ def book(request):
     context = {'form': form}
     return render(request, 'book.html', context)
 
+
 # Add your code here to create new views
+
